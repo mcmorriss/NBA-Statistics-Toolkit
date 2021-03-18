7d0182db1f42c14ae736cfdbf3f8c44130ebe3ff
mcmorriss <michaelmorriss14@gmail.com>
2021-03-18 18:18:36 -0500

    Added multiple features including sorting functions to sorts all stats, and incorporated the compare function to compare multiple players to one another.

--- CompareStats.py
+++ CompareStats.py
@@ -1,41 +1,143 @@
-from playerstats import allstars_2021
+from playerstats import allstars_2021, instruction_list
+from operator import itemgetter
 
-print("                  Hello, and Welcome to the Stat Comparison Program Created by Michael Morriss")
+
+def instructions():
+    print(" ")
+    comp = str(input("Would you like to 'View' or 'Compare' or see the 'List' of players?: "))
+    print(" ")
+    return comp
+
+
+def sort_points(a_list):
+      """Sorts in descending order."""
+      for index in range(1, len(a_list)):
+            value = a_list[index][1]
+            pos = index - 1
+            while pos >= 0 and a_list[pos][1] < value:
+                  a_list[pos + 1] = a_list[pos]
+                  pos -= 1
+            a_list[pos + 1] = value
+
+
+print("Welcome to the NBA All Star Stat Comparison Program")
+print("Programmed by Michael Morriss")
 print(" ")
 print("In this program, you will be able to compare the stats from the 2021 NBA Players that were "
       "selected to be apart of the 2021 NBA All Star Game. ")
-print("The current features allow you to either view individual player stats, or compare the stats of two different players.")
-print(" ")
+print("The current features allow you to either view individual player stats, or compare the stats of "
+      "two different players.")
+
 print("If you would like to view an individual player's stats, then type in 'View'.")
 print("If you would like to compare the stats of two players, then type in 'Compare'.")
+print("If you would like to see the full list of player names, then type in 'List'.")
+print("If you would like to see the full list of usable instructions, then type 'Instructions'.")
 
-comp_type = str(input("Would you like to 'View' or 'Compare'?: "))
-print(" ")
+while True:
+      comp_type = instructions()
 
-if comp_type == 'yes':
-      for i in allstars_2021:
-            print(i[0])
+      if comp_type.lower() == 'instructions':
+            for i in instruction_list:
+                  print(i)
 
-if comp_type.lower() == 'view':
-      print("Please enter the name of the player whose stats you want to view.")
-      print("If you would like to see the full list of player names, then type in 'Player list'.")
-      print(" ")
-      view_player = str(input("Enter the name of a player to view their stats: "))
+      if comp_type.lower() == 'sort points':
+            allstars_2021.sort(key = itemgetter(1), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[1], "PPG")
+      if comp_type.lower() == 'sort rebounds':
+            allstars_2021.sort(key = itemgetter(2), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[2], "RPG")
+      if comp_type.lower() == 'sort assists':
+            allstars_2021.sort(key = itemgetter(3), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[3], "APG")
+      if comp_type.lower() == 'sort blocks':
+            allstars_2021.sort(key = itemgetter(4), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[4], "BPG")
+      if comp_type.lower() == 'sort steals':
+            allstars_2021.sort(key = itemgetter(5), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[5], "SPG")
+      if comp_type.lower() == 'sort fg%':
+            allstars_2021.sort(key = itemgetter(6), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[6], "FG%")
+      if comp_type.lower() == 'sort 3pt%':
+            allstars_2021.sort(key = itemgetter(7), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[7], "3PT%")
+      if comp_type.lower() == 'sort tov':
+            allstars_2021.sort(key = itemgetter(8), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[8], "TOV")
+      if comp_type.lower() == 'sort minutes':
+            allstars_2021.sort(key = itemgetter(9), reverse = True)
+            for i in allstars_2021:
+                  print(i[0] + ":", i[9], "MPG")
 
-      if view_player.lower() == 'list':
+      if comp_type.lower() == 'list':
             for i in allstars_2021:
                   print(i[0])
+
       num_len = range(0, len(allstars_2021))
-      for num in num_len:
-            if view_player.lower() in allstars_2021[num][0].lower():
-                  print(allstars_2021[num][0] + ":")
-                  print("PPG:", allstars_2021[num][1])
-                  print("RPG:", allstars_2021[num][2])
-                  print("APG:", allstars_2021[num][3])
-                  print("BPG:", allstars_2021[num][4])
-                  print("SPG:", allstars_2021[num][5])
-                  print("FG%:", allstars_2021[num][6])
-                  print("3P%:", allstars_2021[num][7])
-                  print("TOV:", allstars_2021[num][8])
-                  print("MPG:", allstars_2021[num][9])
-                  print("Awards:", allstars_2021[num][10])
+
+      if comp_type.lower() == 'awards':
+            play_award = str(input("Enter the player name you would like to view awards for: "))
+            print(" ")
+            for i in num_len:
+                  if play_award.lower() in allstars_2021[i][0].lower():
+                        print(allstars_2021[i][0] + "'s", "Awards: ")
+                        print(allstars_2021[i][10])
+
+      if comp_type.lower() == 'compare':
+            comp_p1 = str(input("Please enter the name of the first player you want to compare: "))
+            comp_p2 = str(input("Please enter the name of the second player you want to compare: "))
+            print(" ")
+            for num in num_len:
+                  if comp_p1.lower() in allstars_2021[num][0].lower():
+                        print(allstars_2021[num][0] + ":", allstars_2021[num][1], "PPG,",
+                              allstars_2021[num][2], "RPG,",allstars_2021[num][3], "APG,",
+                              allstars_2021[num][4], "BPG,", allstars_2021[num][5], "SPG,",
+                              allstars_2021[num][6], "FG%,", allstars_2021[num][7], "3PT%,",
+                              allstars_2021[num][8], "TOV,", allstars_2021[num][9], "MPG")
+                  if comp_p2.lower() in allstars_2021[num][0].lower():
+                        print(allstars_2021[num][0] + ":", allstars_2021[num][1], "PPG,",
+                              allstars_2021[num][2], "RPG,",allstars_2021[num][3], "APG,",
+                              allstars_2021[num][4], "BPG,", allstars_2021[num][5], "SPG,",
+                              allstars_2021[num][6], "FG%,", allstars_2021[num][7], "3PT%,",
+                              allstars_2021[num][8], "TOV,", allstars_2021[num][9], "MPG")
+
+            print(" ")
+
+            for num in num_len:
+                  if comp_p1.lower() in allstars_2021[num][0].lower():
+                        print(allstars_2021[num][0] + "'s Awards:", allstars_2021[num][10])
+                  if comp_p2.lower() in allstars_2021[num][0].lower():
+                        print(allstars_2021[num][0] + "'s Awards:", allstars_2021[num][10])
+
+
+
+
+      if comp_type.lower() == 'view':
+            view_player = str(input("Enter the name of a player to view their stats: "))
+            print("")
+            if view_player.lower() == 'list':
+                  for i in allstars_2021:
+                        print(i[0])
+            num_len = range(0, len(allstars_2021))
+            for num in num_len:
+                  if view_player.lower() in allstars_2021[num][0].lower():
+                        print(allstars_2021[num][0] + ":")
+                        print("PPG:", allstars_2021[num][1])
+                        print("RPG:", allstars_2021[num][2])
+                        print("APG:", allstars_2021[num][3])
+                        print("BPG:", allstars_2021[num][4])
+                        print("SPG:", allstars_2021[num][5])
+                        print("FG%:", allstars_2021[num][6])
+                        print("3P%:", allstars_2021[num][7])
+                        print("TOV:", allstars_2021[num][8])
+                        print("MPG:", allstars_2021[num][9])
+                        print("Awards:", allstars_2021[num][10])
+                        print("")
--- playerstats.py
+++ playerstats.py
@@ -1,17 +1,16 @@
 
 allstars_2021 = [
-    ["Player Name","PPG","RPG","APG","BPG","SPG","FG%","3PT%","TOV","MP",["Awards"],"Team"],
     ["Luka Doncic", 28.5, 8.4, 9.3, 0.7, 1.0, 47.7, 35.7, 4.3, 35.5,["2X All Star, 2019 Rookie Of The Year"]],
     ["Steph Curry", 29.0, 5.3, 6.2, 0.1, 1.4, 47.5, 40.8, 3.1, 33.7,
-     ["2X Most Valuable Player, 3X NBA Champion, 7X All Star, 6X All-NBA, 2016 Scoring Leader, 2016 Steals Leader, 50-40-90 Club"]],
+     ["3X NBA Champion, 2X Most Valuable Player, 7X All Star, 6X All-NBA, 2016 Scoring Leader, 2016 Steals Leader,"
+      " 50-40-90 Club"]],
     ["Giannis Antetokounmpo", 29.1, 11.8, 6.2, 1.3, 1.3, 56.3, 29.5, 3.7, 34.2,
      ["2X Most Valuable Player, 5X All Star, 4X All-NBA, 3X All-Defensive",
       "2020 Defensive Player Of The Year, 2021 All Star MVP, 2017 Most Improved"]],
     ["Nikola Jokic", 26.9, 11.2, 8.6, 0.6, 1.6, 56.5, 41.4, 3.1, 35.7,["3X All Star, 2X All-NBA"]],
     ["LeBron James", 25.5, 8.1, 8.0, 0.6, 1.1, 51.0, 36.5, 3.8, 34.4,
-     ["17X All Star, 16X All-NBA, 4X NBA Champion, 4X Finals MVP, 4X Most Valuable Player",
-      "3X All Star MVP, 2008 Scoring Leader, 2020 Assist Leader, 2004 Rookie Of The Year",
-      "6X All-Defensive"]],
+     ["4X NBA Champion, 4X Finals MVP, 4X Most Valuable Player, 17X All Star, 16X All-NBA,",
+      "6X All-Defensive, 3X All Star MVP, 2008 Scoring Leader, 2020 Assist Leader, 2004 Rookie Of The Year"]],
     ["Chris Paul", 15.9, 4.6, 8.8, 0.3, 1.3, 48.7, 39.0, 2.4, 31.7,
      ["11X All Star, 9X All-NBA, 9X All-Defensive, 6X Steals Leader, 4X Assist Leader",
       "2013 All Star MVP, 2006 Rookie Of The Year"]],
@@ -36,7 +35,7 @@ allstars_2021 = [
     ["Jayson Tatum", 25.3, 6.9, 4.4, 0.5, 1.4, 44.7, 37.3, 2.5, 35.7,["3X All Star, 2X All-NBA"]],
     ["Zion Williamson", 25.6, 6.9, 3.5, 0.7, 0.9, 62.0, 31.6, 2.5, 32.6, ["1X All Star"]],
     ["James Harden", 25.5, 8.2, 11.2, 0.8, 1.2, 47.6, 37.1, 4.3, 38.0,
-     ["9X All Star, 7X All-NBA 3X Scoring Leader, 2018 Most Valuable Player, 2017 Assist Leader,"
+     ["9X All Star, 7X All-NBA, 3X Scoring Leader, 2018 Most Valuable Player, 2017 Assist Leader,"
       " 2012 Sixth Man Of The Year"]],
     ["Donovan Mitchell", 24.6, 4.5, 5.4, 0.3, 0.9, 41.8, 38.4, 3.0, 34.0,["2X All Star"]],
     ["Zach LaVine", 28.4, 5.2, 5.1, 0.5, 1.0, 53.1, 44.1, 3.8, 35.4,["1X All Star"]],
@@ -45,12 +44,31 @@ allstars_2021 = [
     ["Mike Conley", 16.4, 3.3, 5.6, 0.1, 1.4, 45.5, 42.5, 1.9, 29.7,["1X All Star, 1X All-Defensive"]],
     ["Devin Booker", 25.1, 3.7, 4.6, 0.3, 0.8, 49.8, 36.3, 3.4, 33.9,["2X All Star, 70 Point Game"]],
     ["Anthony Davis", 22.5, 8.4, 3.0, 1.8, 1.3, 53.3, 29.3, 2.0, 32.8,
-     ["8X All Star, 2020 NBA Champion, 4X All-NBA. 4X All-Defensive, 3X Blocks Leader, 2017 All Star MVP"]],
+     ["2020 NBA Champion, 8X All Star, 4X All-NBA. 4X All-Defensive, 3X Blocks Leader, 2017 All Star MVP"]],
     ["Kevin Durant", 29.0, 7.3, 5.3, 1.4, 0.7, 52.4, 43.4, 3.5, 35.7,
-     ["11X All Star, 2X NBA Champion, 2X Finals MVP, 4X Scoring Leader, 9X All-NBA, 2X All Star MVP",
-      "2014 Most Valuable Player, 2008 Rookie Of The Year"]],
+     ["2X NBA Champion, 2X Finals MVP, 11X All Star, 4X Scoring Leader, 9X All-NBA, 2X All Star MVP",
+      "2014 Most Valuable Player, 2008 Rookie Of The Year, 50-40-90 Club"]],
     ["Joel Embiid", 29.9, 11.5, 3.3, 1.4, 1.2, 52.5, 42.2, 3.2, 32.6,
-     ["4X All Star, 2X All-NBA, 2X All-Defensive, 2021 Most Valuable Player"]],
+     ["2021 Most Valuable Player, 4X All Star, 2X All-NBA, 2X All-Defensive"]],
     ["Ben Simmons", 15.9, 8.0, 7.8, 0.7, 1.5, 57.1, 37.5, 3.4, 34.0,
      ["3X All Star, 1X All-NBA, 1X All-Defensive, 2020 Steals Leader, 2018 Rookie Of The Year"]]
     ]
+
+instruction_list = [
+    "'View' - view and individual player's stats by entering their name.",
+    "'Compare' - compare the stats and awards between two players entered by the user."
+    "'List' - prints out the total list of available player names to choose from.",
+    "'Awards' - view the list of awards an entered player has achieved during their career tenure."
+    "'Sort points' - prints out a sorted list of player points per game in descending order.",
+    "'Sort rebounds' - prints out a sorted list of player rebs per game in descending order.",
+    "'Sort assists' - prints out a sorted list of player asts per game in descending order.",
+    "'Sort blocks' - prints out a sorted list of player blocks per game in descending order.",
+    "'Sort steals' - prints out a sorted list of player steals per game in descending order.",
+    "'Sort FG%' - prints out a sorted list of player field goal percentage in descending order.",
+    "'Sort 3PT%' - prints out a sorted list of player 3pt percentage in descending order.",
+    "'Sort TOV' - prints out a sorted list of player turnovers per game in descending order.",
+    "'Sort minutes' - prints out a sorted list of player minutes per game in descending order.",
+
+
+
+]
